856b02b3ff6a2db63360b234044a8f6fa02b8eb6
jassemb <benalijassem201@gmail.com>
2023-03-17 17:35:48 +0100

    7-basketball-shots.dart

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,22 +1,7 @@
 int whoWins(Map<String, int> teamA, Map<String, int> teamB){
-  int teamAPoints = calculatePoints(teamA);
-  int teamBPoints = calculatePoints(teamB);
-  
-  //print('Team A points: $teamAPoints');
-  //print('Team B points: $teamBPoints');
-
-  if (teamAPoints > teamBPoints) return 1;
-  if (teamAPoints < teamBPoints) return 2;
-  
+  var team1Total = team1['Free throws']! + team1['2 pointers']! * 2 + team1['3 pointers']! * 3;
+  var team2Total = team2['Free throws']! + team2['2 pointers']! * 2 + team2['3 pointers']! * 3;
+  if (team1Total > team2Total) return 1;
+  if (team1Total < team2Total) return 2;
   return 0;
-}
-
-int calculatePoints(Map<String, int> team) {
-  int freeThrows = team['Free throws'] ?? 0;
-  int twoPointer = (team['2 pointer'] ?? 0) * 2;
-  int threePointer = (team['3 pointer'] ?? 0) * 3;
-  //print('freeThrows: $freeThrows');
-  //print('twoPointer: $twoPointer');
-  //print('threePointer: $threePointer');
-  return freeThrows + twoPointer + threePointer;
 }
