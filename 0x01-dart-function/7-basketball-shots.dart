bca7864ce9392af181b2dbf121e9d6043d906b72
jassemb <benalijassem201@gmail.com>
2023-03-17 17:38:15 +0100

    7-basketball-shots.dart

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,7 +1,6 @@
 int whoWins(Map<String, int> teamA, Map<String, int> teamB){
-  var team1Total = team1['Free throws']! + team1['2 pointers']! * 2 + team1['3 pointers']! * 3;
-  var team2Total = team2['Free throws']! + team2['2 pointers']! * 2 + team2['3 pointers']! * 3;
+  var team1Total = teamA['Free throws']! + teamA['2 pointers']! * 2 + teamA['3 pointers']! * 3;
+  var team2Total = teamB['Free throws']! + teamB['2 pointers']! * 2 + teamB['3 pointers']! * 3;
   if (team1Total > team2Total) return 1;
   if (team1Total < team2Total) return 2;
-  return 0;
-}
+  return 0;}
